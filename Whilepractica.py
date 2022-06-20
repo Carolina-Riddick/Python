d8af819b566cd1b0503756b8d1b07b7a916daae1
Carolina Riddick <carolinariddick@gmail.com>
2022-06-20 00:39:50 -0300

    Ciclo While y For

---
+++ Whilepractica.py
@@ -0,0 +1,13 @@
+from operator import truediv
+
+#maximo = 5
+#contador = 0
+
+#while contador < maximo:
+ #   print("Ejecutando el ciclo while" , contador)
+  #  contador += 1
+#else:
+ #   print("Fin del ciclo while")
+
+ #Ciclo For:
+ 
